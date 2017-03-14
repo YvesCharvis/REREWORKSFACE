13b646f854505d2be3702fd0bcbf15ba51d7b058
Yves Charvis <Yves Charvis>
2017-03-14 09:23:04 +0100

    footer_correct

--- footer.php
+++ footer.php
@@ -1,38 +1,6 @@
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 </div> <!-- ROW SOUS BODY -->
-<footer>
-    <div id="carrousel-id" class="container"  >
-    <div id="single" class="carousel slide" data-ride="carousel" data-shift="1">
-    <h2 class="carrousel-h2">NOS PARTENAIRES</h2>
-    <div class="carousel-inner" >
-      <ul class="row item active" >
-        <li id="ag2r" class="col-xs-4 one"> <a href="http://www.ag2rlamondiale.fr/"><img src="img/carrousel/ag2r.png" class="img-responsive"></a> </li>
-        <li id="admical" class="col-xs-4 one"> <a href="http://www.admical.org/"> <img src="img/carrousel/admical.png" class="img-responsive"></a> </li>
-        <li id="afmd" class="col-xs-4 one"> <a href="http://www.afmd.fr/"><img src="img/carrousel/afmd.png" class="img-responsive"></a> </li>
-        </ul>
-        <ul class="row item">
-          <li id="anru" class="col-xs-4 one"><a href="http://www.anru.fr/"> <img src="img/carrousel/anru.png" class="img-responsive"></a> </li> 
-          <li id="apf" class="col-xs-4 one"> <a href="http://www.apf.asso.fr/"><img src="img/carrousel/logoapf.png" class="img-responsive"></a> </li>
-        </ul>
-        <ul class="row item">
-          <li id="cnam" class="col-xs-4 one"><a href="http://www.cnam.fr//"> <img src="img/carrousel/logo_cnam.png" class="img-responsive"></a> </li>
-          <li id="fei" class="col-xs-4 one"><a href="https://www.fei.org/"> <img src="img/carrousel/fei.png" class="img-responsive"></a> </li>
-        </ul>
-        <ul class="row item">
-          <li id="total" class="col-xs-4 one"><a href="http://www.ag2rlamondiale.fr/"> <img src="img/carrousel/total.png" class="img-responsive"></a> </li>
-          <li id="union" class="col-xs-4 one"><a href="http://www.ag2rlamondiale.fr/"> <img src="img/carrousel/union.png" class="img-responsive"></a> </li>
-        </ul>
-      </div>
-      <a class="carousel-control left" href="#single" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a> 
 
-      <a class="carousel-control right" href="#single" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-      </div>
-      </div>
-  </footer>
 
    <!-- BasDePage
 ================== -->
@@ -95,9 +63,44 @@
         <!--/.container--> 
     </div>
     <!--/.footer-->
+   
 
     </div>
     <!--/.footer-bottom--> 
+    <div id="carrousel-id" class="container"  >
+    <div id="single" class="carousel slide" data-ride="carousel" data-shift="1">
+    <h2 class="carrousel-h2">NOS PARTENAIRES</h2>
+    <div class="carousel-inner" >
+      <ul class="row item active" >
+        <li id="ag2r" class="col-xs-4 one"> <a href="http://www.ag2rlamondiale.fr/"><img src="img/carrousel/ag2r.png" class="img-responsive"></a> </li>
+        <li id="admical" class="col-xs-4 one"> <a href="http://www.admical.org/"> <img src="img/carrousel/admical.png" class="img-responsive"></a> </li>
+        <li id="afmd" class="col-xs-4 one"> <a href="http://www.afmd.fr/"><img src="img/carrousel/afmd.png" class="img-responsive"></a> </li>
+        </ul>
+        <ul class="row item">
+          <li id="anru" class="col-xs-4 one"><a href="http://www.anru.fr/"> <img src="img/carrousel/anru.png" class="img-responsive"></a> </li> 
+          <li id="apf" class="col-xs-4 one"> <a href="http://www.apf.asso.fr/"><img src="img/carrousel/logoapf.png" class="img-responsive"></a> </li>
+        </ul>
+        <ul class="row item">
+          <li id="cnam" class="col-xs-4 one"><a href="http://www.cnam.fr//"> <img src="img/carrousel/logo_cnam.png" class="img-responsive"></a> </li>
+          <li id="fei" class="col-xs-4 one"><a href="https://www.fei.org/"> <img src="img/carrousel/fei.png" class="img-responsive"></a> </li>
+        </ul>
+        <ul class="row item">
+          <li id="total" class="col-xs-4 one"><a href="http://www.ag2rlamondiale.fr/"> <img src="img/carrousel/total.png" class="img-responsive"></a> </li>
+          <li id="union" class="col-xs-4 one"><a href="http://www.ag2rlamondiale.fr/"> <img src="img/carrousel/union.png" class="img-responsive"></a> </li>
+        </ul>
+      </div>
+      <a class="carousel-control left" href="#single" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a> 
+
+      <a class="carousel-control right" href="#single" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+      </div>
+      </div>
+  </footer>
+
+
 </footer>
    
 <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script> 
